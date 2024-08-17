4afa13726cfbb615dfce85b2da31ae94f0d029e5
hanzla sibghat <70375791+hanzla-sib@users.noreply.github.com>
2024-08-17 15:57:05 +0500

    main layout of Learn done

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,13 +1,16 @@
+
+import { MobileHeader } from "@/components/mobileHeader";
+import { Sidebar } from "@/components/sidebar";
 type Props = {
   children: React.ReactNode;
 };
 
-import { Sidebar } from "@/components/sidebar";
 const MainLayout = ({ children }: Props) => {
   return (
     <>
-      <Sidebar />
-      <main className="pl-[256px] h-full">
+      <MobileHeader />
+      <Sidebar className="hidden lg:flex" />
+      <main className="lg:pl-[256px] h-full pt-[50px] lg:pt-0">
         <div className="bg-red-500 h-full">{children}</div>
       </main>
     </>
