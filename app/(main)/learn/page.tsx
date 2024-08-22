df2abfe801527dc11fdf5568aae234c3c72d4dc0
hanzla sibghat <70375791+hanzla-sib@users.noreply.github.com>
2024-08-22 18:26:50 +0500

    loader done

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,10 +1,17 @@
+import { redirect } from "next/navigation";
 import { FeedWrapper } from "@/components/feed-wrapper";
 import { StickyWrapper } from "@/components/sticky-wrapper";
 import React from "react";
 import { Header } from "./Header";
 import { UserProgress } from "@/components/userProgress";
+import { getUserProgress } from "@/db/queries";
 
-const Learn = () => {
+const Learn = async () => {
+  const userProgressData = getUserProgress();
+  const [userProgress] = await Promise.all([userProgressData]);
+  if (!userProgress || !userProgress.activeCourse) {
+    redirect("/courses");
+  }
   return (
     <div className="flex flex-row-reverse gap-[48px] px-6">
       <StickyWrapper>
@@ -31,9 +38,6 @@ const Learn = () => {
           <div className="h-[700px] bg-blue-500 w-full"></div>
           <div className="h-[700px] bg-blue-500 w-full"></div>
           <div className="h-[700px] bg-blue-500 w-full"></div>
-          
-        
-
         </div>
       </FeedWrapper>
     </div>
