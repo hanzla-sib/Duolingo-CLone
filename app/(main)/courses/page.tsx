5536c24f2750554bf6cccceaeb25ff1af680aab2
hanzla sibghat <70375791+hanzla-sib@users.noreply.github.com>
2024-08-27 23:11:27 +0500

    s

--- app/(main)/courses/page.tsx
+++ app/(main)/courses/page.tsx
@@ -6,7 +6,7 @@ const CoursesPage = async () => {
   const userProgress = await getUserProgress();
   return (
     <div className="h-full mx-auto max-w-[912px] px-3 ">
-      <h1 className="text-wcl font-bold text-neutral-700">Langauge Courses</h1>
+      <h1 className="text-wcl font-bold text-neutral-700">langauge Courses</h1>
       <List course={courses} activeCourseId={userProgress?.activeCourseId} />
       {/* {JSON.stringify(data)} */}
     </div>
